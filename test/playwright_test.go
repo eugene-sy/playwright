bca98da2ad285e01ccd2a7ca7c760a8b16d3520c
Eugene Sypachev (@Axblade) <eugene.sypachev@gmail.com>
2018-12-18 21:14:08 +0300

    test help and parameterless invocation

--- test/playwright_test.go
+++ test/playwright_test.go
@@ -7,7 +7,10 @@ import (
 )
 
 var (
-	binary = "../playwright"
+	binary   = "../playwright"
+	commands = []string{
+		"create", "update", "delete",
+	}
 )
 
 func TestVersionCall(t *testing.T) {
@@ -22,5 +25,49 @@ func TestVersionCall(t *testing.T) {
 
 	if _, err := regexp.MatchString("^\\d+\\.\\d+\\.\\d+$", outStr); err != nil {
 		t.Error("'playwright --version' output is not a correct semver: ", outStr)
+	}
+}
+
+func TestHelpCall(t *testing.T) {
+	cmd := exec.Command(binary, "help")
+	out, err := cmd.CombinedOutput()
+	outStr := string(out)
+
+	checkHelpOutput(t, outStr, err)
+}
+
+func TestHelpOptionCall(t *testing.T) {
+	cmd := exec.Command(binary, "--help")
+	out, err := cmd.CombinedOutput()
+	outStr := string(out)
+
+	checkHelpOutput(t, outStr, err)
+}
+
+func checkHelpOutput(t *testing.T, outStr string, err error) {
+	if err != nil {
+		t.Error("'playwright help' failed with ", err)
+	}
+
+	if _, err := regexp.MatchString("^usage: playwright [<flags>] <command> [<args> ...]", outStr); err != nil {
+		t.Error("'playwright help' output is not a correct semver: ", outStr)
+	}
+}
+
+func TestParameterlessInvocation(t *testing.T) {
+	for _, command := range commands {
+		cmd := exec.Command(binary, command)
+		out, err := cmd.CombinedOutput()
+
+		if err == nil {
+			outStr := string(out)
+			t.Error("'playwright ", command, "' was expected to fail, but it succeded with output: ", outStr)
+		}
+
+		errStr := err.Error()
+		if _, matchErr := regexp.MatchString("required argument 'name' not provided", errStr); matchErr != nil {
+
+			t.Error("'playwright ", command, "' was expected to fail with suggestion to add 'name' parameter, but output was: ", errStr)
+		}
 	}
 }
